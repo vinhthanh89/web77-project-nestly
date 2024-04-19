80623d7cff84153eb0d04564e9ad568f7ccd3d62
vinhthanh89 <vinhthanh89@gmail.com>
2024-04-19 22:04:10 +0700

    update BackEnd

--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -2,10 +2,12 @@ import {Router} from "express"
 
 
 import userRouter from "./user.router.js"
+import productRouter from "./product.router.js"
 
 const router = Router()
 
 router.use("/user" , userRouter)
+router.use('/product' , productRouter)
 
 
 export default router
--- FrontEnd/src/pages/LandingPage/index.jsx
+++ FrontEnd/src/pages/LandingPage/index.jsx
@@ -1,8 +1,11 @@
 const LandingPage = () => {
   return (
     <>
-      <div className="w-screen h-screen">
+      <div className="w-full h-screen">
         <h1>Landing page</h1>
+        <img
+        className="w-full h-full object-cover"
+         src="https://images.unsplash.com/photo-1601918774946-25832a4be0d6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
       </div>
     </>
   );
