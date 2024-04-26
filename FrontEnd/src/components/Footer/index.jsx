bc93809b8e784a05baaf5a23167db7e61883ca71
iamchouty <quocchau4729@gmail.com>
2024-04-26 23:54:55 +0700

    FrontEnd: Landing page - 90%

--- FrontEnd/src/components/Footer/index.jsx
+++ FrontEnd/src/components/Footer/index.jsx
@@ -1,8 +1,38 @@
+import "./index.css";
 const Footer = () => {
   return (
     <>
-      <div className="w-full bg-transparent">
-        <h1>Footer</h1>
+      <div className="w-full bg-black h-[50vh]">
+        <div className="footer-holder w-full h-[70%] flex justify-center">
+          <div className="footer w-[90%]">
+            <div className="h-full flex items-center">
+              <img src="/assets/Logo_white.png" alt="" className="w-[5rem]" />
+              <h1 className="logo text-4xl text-white font-black ml-[1rem] mt-[1rem]">
+                Nestly
+              </h1>
+            </div>
+            <div className="h-full w-full flex justify-center items-end flex-col pt-[5rem]">
+              <div>
+                <h1 className="text-xl text-white font-semibold">Contact us</h1>
+                <p className="text-lg text-white font-light mt-3">
+                  Email: nestly@info.com
+                </p>
+                <p className="text-lg text-white font-light mt-3">
+                  Phone: 0909090909
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-[30%] flex justify-between items-center pl-[4rem] pr-[4rem]">
+          <div className="copy-right pl-[1rem]">
+            <p>Copyright©2024 Nestly. All Rights Reserved.</p>
+          </div>
+          <div className="flex">
+            <button className="pr-[2rem]">Terms and Conditions</button>
+            <button className="pr-[1rem]">Privacy Policy</button>
+          </div>
+        </div>
       </div>
     </>
   );
--- FrontEnd/src/pages/LandingPage/index.jsx
+++ FrontEnd/src/pages/LandingPage/index.jsx
@@ -1,24 +1,106 @@
 import { Link } from "react-router-dom";
-import AboutUs from "../../components/AboutUs";
 import Footer from "../../components/Footer";
 import NavigationBar from "../../components/NavigationBar";
-import Places from "../../components/Places";
+import { FaLongArrowAltRight } from "react-icons/fa";
 import "./index.css";
 const LandingPage = () => {
   return (
     <>
       <div className="landing-page">
-        <NavigationBar />
-        <div className="quote flex justify-center items-center bg-transparent h-2/3 ">
-          <ul className="flex-wrap justify-center items-center bg-transparent">
-            <p className="bg-transparent text-7xl text-white">
-              Affordable, Diverse, Convenient
-            </p>
-          </ul>
+        {/* Top container */}
+
+        <div className="top-container h-[100vh]">
+          <NavigationBar />
+          <div className="quote mt-[12rem]">
+            <ul className="flex-col justify-center">
+              <h1 className="flex justify-center neon_1 text-7xl text-white">
+                Affordable Diverse Convenient
+              </h1>
+              <p className="flex justify-center neon_2 text-xl text-black font-semibold mt-8">
+                Welcome to Nestly, a web platform designed for those seeking a
+                place they can call HOME
+              </p>
+            </ul>
+            {/* Button */}
+            <div className="btn-holder w-full flex justify-center mt-8">
+              <button className="skeleton btn md:btn-md text-white">
+                <FaLongArrowAltRight />
+                <Link>
+                  <p className="pb-1">Try Nestly now!</p>
+                </Link>
+              </button>
+            </div>
+          </div>
         </div>
-        {/* <Places />
-          <AboutUs />
-          <Footer /> */}
+        {/* About us */}
+        <div className="bg-white flex justify-center">
+          <div className="w-[90%] flex justify-center bot">
+            {/* about us */}
+            <div className="w-1/2 pt-[5rem] pr-[3rem]">
+              <h3 className="text-xl text-blue-500 font-bold">About us</h3>
+              <h1 className="text-5xl text-black font-medium mt-[1rem]">
+                Who are we?
+              </h1>
+              <p className="text-lg mt-5 font-medium leading-10">
+                We are Nestly! A dedicated team that providing homes and
+                apartments that not only offer unparalleled comfort and
+                affordability but also resonate with warmth and a sense of
+                belonging. With a commitment to fostering communities and
+                enriching lives, Nestly welcomes you to explore our selection of
+                thoughtfully designed to elevate your living experience.
+              </p>
+            </div>
+            {/* image */}
+            <div>
+              <img
+                src="/assets/aboutus.jpg"
+                alt=""
+                className="w-[30rem] h-[30rem]"
+              />
+            </div>
+          </div>
+        </div>
+        {/* information */}
+        <div className="section bg-white pt-[7rem] pb-[10rem]">
+          <h3 className="flex justify-center text-xl text-blue-500 font-bold">
+            Why choose us
+          </h3>
+          <div className="small-sections w-full flex justify-center">
+            <div className="img-holder w-1/3">
+              <h1 className="text-5xl text-black font-medium mt-[1rem]">
+                Sustainable
+              </h1>
+              <img src="/assets/Display_4.jpg" alt="" />
+              <p className="text-center text-lg mt-5 font-medium leading-10">
+                We prioritize sustainability in every home, offering
+                eco-friendly options without compromising comfort.
+              </p>
+            </div>
+
+            <div className="img-holder w-1/3">
+              <h1 className="text-5xl text-black font-medium mt-[1rem]">
+                Affordable
+              </h1>
+              <img src="/assets/Display_5.jpg" alt="" />
+              <p className="text-center text-lg mt-5 font-medium leading-10">
+                Range of affordable options makes homeownership achievable
+                without sacrificing quality and income.
+              </p>
+            </div>
+
+            <div className="img-holder w-1/3">
+              <h1 className="text-5xl text-black font-medium mt-[1rem]">
+                Happiness
+              </h1>
+              <img src="/assets/Display_2.jpg" alt="" />
+              <p className="text-center text-lg mt-5 font-medium leading-10">
+                We believe in creating environments where families can thrive
+                and make lasting memories, fostering joy.
+              </p>
+            </div>
+          </div>
+        </div>
+        <Footer />
       </div>
     </>
   );
