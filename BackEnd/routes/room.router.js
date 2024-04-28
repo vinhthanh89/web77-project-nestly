4c151a46f08901e69c6c929542f09031e1047aeb
vinhthanh89 <149566726+vinhthanh89@users.noreply.github.com>
2024-04-28 16:14:25 +0700

    fix placeholder

--- BackEnd/routes/room.router.js
+++ BackEnd/routes/room.router.js
@@ -4,12 +4,16 @@ import {
   getRooms,
   filterRoomByCity,
   filterRoomsByType,
+  getRoomById,
+  getCityOptions,
 } from "../controllers/room.controller.js";
 import { authentication } from "../middlewares/authentication.js";
 
 const router = Router();
 
 router.get("/get-rooms", getRooms);
+router.get('/get-rooms/:id' , getRoomById)
+router.get('/get-city-options' , getCityOptions)
 router.get("/get-paging-rooms", getPagingRooms);
 router.get("/filterByCity", filterRoomByCity);
 router.get("/filterByType", filterRoomsByType);
--- FrontEnd/src/services/room.js
+++ FrontEnd/src/services/room.js
@@ -5,3 +5,7 @@ import {axiosInstance} from './index'
 export const fetchRoomData = async () => {
     return axiosInstance.get('/room/get-rooms')
 }
+
+export const fetchCityOptions = async () => {
+    return axiosInstance.get('/room/get-city-options')
+}
