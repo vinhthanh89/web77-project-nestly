5fdd1657c17e23b82f4bad8cc48bdae7a731743f
iamchouty <quocchau4729@gmail.com>
2024-05-01 13:10:31 +0700

    Hello

--- FrontEnd/src/pages/CardDetails/index.jsx
+++ FrontEnd/src/pages/CardDetails/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "./../../components/Header/index";
 import Footer from "../../components/Footer";
@@ -106,7 +106,6 @@ const CardDetails = () => {
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
